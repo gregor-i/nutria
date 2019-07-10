2071d82c3e133ace4e7c7e13719f3e2f43d8ef26
gregor-i <gregor_alias1@outlook.de>
2019-07-10 14:53:56 +0200

    more complex number functions

--- frontend/src/main/scala/nutria/frontend/shaderBuilder/GlobalDefintions.scala
+++ frontend/src/main/scala/nutria/frontend/shaderBuilder/GlobalDefintions.scala
@@ -7,9 +7,17 @@ object GlobalDefintions {
       |#define complex_product(a, b) vec2(a.x*b.x-a.y*b.y, a.x*b.y+a.y*b.x)
       |#define complex_conjugate(a) vec2(a.x,-a.y)
       |#define complex_divide(a, b) vec2(((a.x*b.x+a.y*b.y)/(b.x*b.x+b.y*b.y)),((a.y*b.x-a.x*b.y)/(b.x*b.x+b.y*b.y)))
+      |#define complex_power(a, b) vec2(pow(length(a), b.x) / exp(atan(a.x, a.y) * b.y), atan(a.x, a.y)*b.x + log(length(a)) * b.y)
+      |
       |#define complex_exp(a) exp(a.x) * vec2(cos(a.y), sin(a.y))
       |#define complex_sq(a) complex_product(a, a)
+      |#define complex_sin(a) vec2(sin(a.x)*cosh(a.y), cos(a.x)*sinh(a.y))
+      |#define complex_cos(a) vec2(cos(a.x)*cosh(a.y), -sin(a.x)*sinh(a.y))
+      |#define complex_tan(a) vec2(sin(2.0*a.x), sinh(2.0*a.y)) / (cos(2.0*a.x) + cosh(2.0*a.y))
+      |#define complex_log(a) vec2(length(a), atan(a.x, a.y))
       |
     """.stripMargin
 
+
+  import spire.math.Complex
 }
--- frontend/src/main/scala/nutria/frontend/shaderBuilder/NewtonLang.scala
+++ frontend/src/main/scala/nutria/frontend/shaderBuilder/NewtonLang.scala
@@ -16,11 +16,21 @@ object NewtonLang {
         case Times => s"complex_product(vec2($left), vec2($right))"
         case Divided => s"complex_divide(vec2($left), vec2($right))"
         case Power if right == "vec2(float(2), float(0))" => s"complex_sq(vec2($left))"
-        case Power if right == "vec2(float(1), float(0))" => left
-        case Power => println(right); ???
+        case Power => s"complex_power($left, $right)"
       },
       ifUnitary = (op, child) => op match {
+        case Neg => s"-($child)"
+        case Sin => s"complex_sin(vec2($child))"
+        case Cos => s"complex_cos(vec2($child))"
+        case Tan => s"complex_tan(vec2($child))"
+        case Asin => ???
+        case Acos => ???
+        case Atan => ???
+        case Sinh => ???
+        case Cosh => ???
+        case Tanh => ???
         case Exp => s"complex_exp(vec2($child))"
+        case Log => s"complex_log(vec2($child))"
       },
       ifVariable = varsToCode
     )
