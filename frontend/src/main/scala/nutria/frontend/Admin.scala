c4e23d09de604e01d50309e71e9046c516354074
Gregor Ihmor <gregor.ihmor@gmail.com>
2019-10-04 16:16:42 +0200

    scalajs in admin

--- frontend/src/main/scala/nutria/frontend/Admin.scala
+++ frontend/src/main/scala/nutria/frontend/Admin.scala
@@ -11,48 +11,73 @@ import scala.concurrent.Future
 
 object Admin {
   def setup(): Unit = {
-    Untyped(dom.window).putFractalImage = (fractalId: String) => {
-      (for {
-        fractal <- NutriaService.loadFractal(fractalId)
-        canvas <- Future {
-          val canvas = dom.document.createElement("canvas").asInstanceOf[Canvas]
-          canvas.setAttribute("width", "400")
-          canvas.setAttribute("height", "225")
-          canvas
-        }
-        _ = FractalRenderer.render(canvas, fractal, false)
-        url = Untyped(canvas).toDataURL("image/png").asInstanceOf[String]
-        post <- Ajax.put(
-          url = s"/api/fractals/${fractalId}/image",
+    Untyped(dom.window).putFractalImage = putFractalImage
+    Untyped(dom.window).putAllFractalImages = putAllFractalImages
+    Untyped(dom.window).cleanFractals = cleanFractals
+    Untyped(dom.window).truncateImages = truncateImages
+    Untyped(dom.window).insertSystemFractals = insertSystemFractals
+    Untyped(dom.window).deleteFractal = deleteFractal
+    println("Admin Setup completed")
+  }
+
+  val putFractalImage: String => Future[Unit] = (fractalId: String) =>
+    for {
+      fractal <- NutriaService.loadFractal(fractalId)
+      canvas <- Future {
+        val canvas = dom.document.createElement("canvas").asInstanceOf[Canvas]
+        canvas.setAttribute("width", "400")
+        canvas.setAttribute("height", "225")
+        canvas
+      }
+      _ = FractalRenderer.render(canvas, fractal, false)
+      url = Untyped(canvas).toDataURL("image/png").asInstanceOf[String]
+      _ <- Ajax.put(
+        url = s"/api/fractals/${fractalId}/image",
+        headers = Map("Content-Type" -> "image/png"),
+        data = url.stripPrefix("data:image/png;base64,")
+      )
+      _ <- onFinished
+    } yield ()
+
+  val putAllFractalImages: Unit => Future[Unit] = _ =>
+    for {
+      fractals <- NutriaService.loadFractals()
+      canvas <- Future {
+        val canvas = dom.document.createElement("canvas").asInstanceOf[Canvas]
+        canvas.setAttribute("width", "400")
+        canvas.setAttribute("height", "225")
+        canvas
+      }
+      _ <- Future.sequence {
+        for {
+          fractal <- fractals
+          _ = FractalRenderer.render(canvas, fractal.entity, false)
+          url = Untyped(canvas).toDataURL("image/png").asInstanceOf[String]
+        } yield Ajax.put(
+          url = s"/api/fractals/${fractal.id}/image",
           headers = Map("Content-Type" -> "image/png"),
           data = url.stripPrefix("data:image/png;base64,")
         )
-      } yield post.responseText)
-        .onComplete(println)
-    }
-    Untyped(dom.window).putAllFractalImages = () => {
-      (for {
-        fractals <- NutriaService.loadFractals()
-        canvas <- Future {
-          val canvas = dom.document.createElement("canvas").asInstanceOf[Canvas]
-          canvas.setAttribute("width", "400")
-          canvas.setAttribute("height", "225")
-          canvas
-        }
-        _ <- Future.sequence {
-          for {
-            fractal <- fractals
-            _ = FractalRenderer.render(canvas, fractal.entity, false)
-            url = Untyped(canvas).toDataURL("image/png").asInstanceOf[String]
-          } yield Ajax.put(
-            url = s"/api/fractals/${fractal.id}/image",
-            headers = Map("Content-Type" -> "image/png"),
-            data = url.stripPrefix("data:image/png;base64,")
-          )
-        }
-      } yield s"${fractals.size} Fractals rendered")
-        .onComplete(println)
-    }
-    println("Admin Setup completed")
-  }
+      }
+      _ <- onFinished
+    } yield ()
+
+  val cleanFractals: Unit => Future[Unit] = _ =>
+    Ajax.post(url = "/admin/clean-fractals")
+      .flatMap(_ => onFinished)
+
+  val truncateImages: Unit => Future[Unit] = _ =>
+    Ajax.post(url = "/admin/truncate-images")
+      .flatMap(_ => onFinished)
+
+  val insertSystemFractals: Unit => Future[Unit] = _ =>
+    Ajax.post(url = "/admin/insert-system-fractals")
+      .flatMap(_ => onFinished)
+
+  val deleteFractal: String => Future[Unit] = id =>
+    Ajax.post(url = s"/admin/delete-fractal/$id")
+      .flatMap(_ => onFinished)
+
+
+  def onFinished = Future(dom.window.location.reload())
 }
