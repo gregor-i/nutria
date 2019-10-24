9d0fc81bf52f7fc1cced7c0d2e9d9ef2153fd1e8
Gregor Ihmor <gregor.ihmor@gmail.com>
2019-10-24 20:40:22 +0200

    use font awesome

--- frontend/src/main/scala/nutria/frontend/common/Buttons.scala
+++ frontend/src/main/scala/nutria/frontend/common/Buttons.scala
@@ -4,16 +4,16 @@ import snabbdom.Snabbdom.h
 import snabbdom.{SnabbdomNative, VNode}
 
 object Buttons {
-  def apply(text: String, imgSrc: String,
-                    onclick: SnabbdomNative.Eventlistener,
-                    `class`: String = "",
-                    disabled: Boolean = false) =
+  def apply(text: String, icon: String,
+            onclick: SnabbdomNative.Eventlistener,
+            `class`: String = "",
+            disabled: Boolean = false) =
     h("button.button" + `class`,
       events = Seq("click" -> onclick),
       attrs = if (disabled) Seq("disabled" -> "true") else Seq.empty
     )(
       h("span.icon")(
-        h("img", attrs = Seq("src" -> imgSrc))()
+        h(s"i.fa.fa-$icon")()
       ),
       h("span")(text)
     )
--- frontend/src/main/scala/nutria/frontend/common/Images.scala
+++ frontend/src/main/scala/nutria/frontend/common/Images.scala
@@ -1,12 +1,10 @@
 package nutria.frontend.common
 
 object Images {
-  val upload = "/img/outline-cloud_upload-24px.svg"
-  val edit = "/img/outline-edit-24px.svg"
-  val explore = "/img/outline-explore-24px.svg"
-  val info = "/img/outline-info-24px.svg"
-  val check = "/img/outline-check-24px.svg"
-  val cancel = "/img/outline-cancel-24px.svg"
-  val share = "/img/outline-share-24px.svg"
-  val delete = "/img/outline-delete-24px.svg"
+  val upload = "upload"
+  val edit = "edit"
+  val info = "info-circle"
+  val check = "check-circle"
+  val cancel = "times-circle"
+  val delete = "trash"
 }
