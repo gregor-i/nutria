9d44cd0c36238f45713f740889ddc13466c1a686
Gregor Ihmor <gregor.ihmor@gmail.com>
2019-10-29 16:46:23 +0100

    fixed offscreencanvas rendering ...

--- frontend/src/main/scala/nutria/frontend/common/FractalImage.scala
+++ frontend/src/main/scala/nutria/frontend/common/FractalImage.scala
@@ -4,7 +4,7 @@ import nutria.core.{Dimensions, FractalEntity}
 import nutria.frontend.shaderBuilder.FractalRenderer
 import nutria.frontend.util.Untyped
 import org.scalajs.dom
-import org.scalajs.dom.html.Canvas
+import org.scalajs.dom.html.{Canvas, Image}
 import org.scalajs.dom.raw.WebGLRenderingContext
 import snabbdom.Snabbdom.h
 import snabbdom.{Snabbdom, SnabbdomNative, VNode}
@@ -12,18 +12,32 @@ import snabbdom.{Snabbdom, SnabbdomNative, VNode}
 import scala.scalajs.js.Dynamic
 
 object FractalImage {
-  private val untypedWindow = Untyped(dom.window)
-  private val offscreenCanvas = Dynamic.newInstance(untypedWindow.OffscreenCanvas)(0, 0)
-  private val webglCtx = Untyped(offscreenCanvas).getContext("webgl").asInstanceOf[WebGLRenderingContext]
+  // TODO: somehow OffscrencanvasStrategy doesn't work on chromium ....
+  private val strategy = ImgStrategy
+  //    if (Untyped(dom.window).OffscreenCanvas.asInstanceOf[UndefOr[_]].isDefined)
+//      OffscrencanvasStrategy
+//    else
+//      ImgStrategy
 
   def apply(fractalEntity: FractalEntity, dimensions: Dimensions): VNode =
+    strategy.render(fractalEntity, dimensions)
+}
+
+private sealed trait Strategy {
+  def render(fractalEntity: FractalEntity, dimensions: Dimensions): VNode
+}
+
+private object OffscrencanvasStrategy extends Strategy {
+  private lazy val untypedWindow = Untyped(dom.window)
+  private lazy val offscreenCanvas = Dynamic.newInstance(untypedWindow.OffscreenCanvas)(0, 0)
+  private lazy val webglCtx = Untyped(offscreenCanvas).getContext("webgl").asInstanceOf[WebGLRenderingContext]
+
+  override def render(fractalEntity: FractalEntity, dimensions: Dimensions): VNode =
     h("canvas",
+      attrs = Seq("width" -> dimensions.width.toString, "height" -> dimensions.height.toString),
       hooks = Seq[(String, SnabbdomNative.Hook)](
         "insert" -> Snabbdom.hook { node =>
           val canvas = node.elm.get.asInstanceOf[Canvas]
-          canvas.width = dimensions.width
-          canvas.height = dimensions.height
-
           dom.window.setTimeout(() => {
             val webGlProgram = FractalRenderer.constructProgram(webglCtx, fractalEntity.program, fractalEntity.antiAliase)
             offscreenCanvas.width = dimensions.width
@@ -33,4 +47,26 @@ object FractalImage {
           }, 5)
         }
       ))()
+}
+
+private object ImgStrategy extends Strategy {
+  private lazy val canvas: Canvas = dom.document.createElement("canvas").asInstanceOf[Canvas]
+  private lazy val webglCtx = canvas.getContext("webgl").asInstanceOf[WebGLRenderingContext]
+
+  override def render(fractalEntity: FractalEntity, dimensions: Dimensions): VNode =
+    h("img",
+      attrs = Seq("width" -> dimensions.width.toString, "height" -> dimensions.height.toString),
+      hooks = Seq[(String, SnabbdomNative.Hook)](
+        "insert" -> Snabbdom.hook { node =>
+          val img = node.elm.get.asInstanceOf[Image]
+          dom.window.setTimeout(() => {
+            canvas.width = dimensions.width
+            canvas.height = dimensions.height
+            val webGlProgram = FractalRenderer.constructProgram(webglCtx, fractalEntity.program, fractalEntity.antiAliase)
+            FractalRenderer.render(webglCtx, fractalEntity.view, webGlProgram)
+            img.src = canvas.toDataURL("image/png")
+          }, 5)
+        }
+      )
+    )()
 }
