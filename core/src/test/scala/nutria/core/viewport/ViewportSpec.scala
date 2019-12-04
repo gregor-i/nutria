3323c773c794ca173aff6fd801dcc061a2f5d490
Gregor Ihmor <gregor.ihmor@gmail.com>
2019-12-04 21:34:22 +0100

    dependancy updates

--- core/src/test/scala/nutria/core/viewport/ViewportSpec.scala
+++ core/src/test/scala/nutria/core/viewport/ViewportSpec.scala
@@ -1,11 +1,12 @@
 package nutria.core.viewport
 
 import org.scalacheck.Gen.choose
-import org.scalacheck.Prop.forAll
-import org.scalatest.{Assertion, FunSuite, Matchers}
-import org.scalatestplus.scalacheck.Checkers
+import org.scalacheck.Prop.{forAll, _}
+import org.scalatest.Assertion
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers
 
-class ViewportSpec extends FunSuite with Matchers with Checkers {
+class ViewportSpec extends AnyFunSuite with Matchers {
   def beClose(left: Viewport, right: Viewport): Assertion = {
     left.origin._1 shouldBe (right.origin._1 +- 0.01)
     left.origin._2 shouldBe (right.origin._2 +- 0.01)
@@ -16,22 +17,22 @@ class ViewportSpec extends FunSuite with Matchers with Checkers {
   }
 
   test("Viewport should have invertible move Operations") {
-    check(forAll(ViewportChooser.chooseViewport, choose(-10d, 10d)) {
+    forAll(ViewportChooser.chooseViewport, choose(-10d, 10d)) {
       (viewport, factor) =>
         beClose(viewport.right(factor).left(factor), viewport)
         beClose(viewport.left(factor).right(factor), viewport)
         beClose(viewport.up(factor).down(factor), viewport)
         beClose(viewport.down(factor).up(factor), viewport)
         true
-    })
+    }
   }
 
   test("Viewport should have invertible zoom Operations") {
-    check(forAll(ViewportChooser.chooseViewport, choose(-10d, 10d), choose(0d, 1d), choose(0d, 1d)) {
+    forAll(ViewportChooser.chooseViewport, choose(-10d, 10d), choose(0d, 1d), choose(0d, 1d)) {
       (viewport, factor, zoomX, zoomY) =>
         beClose(viewport.zoomIn((zoomX, zoomY)).zoomOut((zoomX, zoomY)), viewport)
         beClose(viewport.zoomOut((zoomX, zoomY)).zoomIn((zoomX, zoomY)), viewport)
         true
-    })
+    }
   }
 }
