0ef24add1dfb82f4de4a685962479418016ebc9e
Gregor Ihmor <gregor.ihmor@gmail.com>
2020-01-25 11:57:23 +0100

    manifest and service worker prototype

--- service/app/controller/UiController.scala
+++ service/app/controller/UiController.scala
@@ -4,8 +4,20 @@ import controllers.Assets
 import javax.inject.Inject
 import play.api.mvc.InjectedController
 
-class UiController @Inject() (assets: Assets) extends InjectedController {
+import scala.concurrent.ExecutionContext
+
+class UiController @Inject() (assets: Assets)(implicit ex: ExecutionContext) extends InjectedController {
   def frontend(path: String) = asset("nutria.html", "/public/html")
 
   def asset(file: String, folder: String) = assets.at(folder, file)
+
+  def serviceWorker(file: String, folder: String) =
+    Action.async(
+      rq =>
+        assets
+          .at(folder, file)
+          .apply(rq)
+          .map(_.withHeaders("Service-Worker-Allowed" -> "/"))
+    )
+
 }
