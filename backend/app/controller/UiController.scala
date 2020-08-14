f92a8b9376bd387b69e3e6b1f491823226997e68
Gregor Ihmor <gregor.ihmor@gmail.com>
2020-08-15 00:47:58 +0200

    disable service worker in dev and test mode

--- backend/app/controller/UiController.scala
+++ backend/app/controller/UiController.scala
@@ -2,22 +2,27 @@ package controller
 
 import controllers.Assets
 import javax.inject.Inject
+import play.api.{Environment, Mode}
 import play.api.mvc.InjectedController
 
 import scala.concurrent.ExecutionContext
 
-class UiController @Inject() (assets: Assets)(implicit ex: ExecutionContext) extends InjectedController {
+class UiController @Inject() (assets: Assets, env: Environment)(implicit ex: ExecutionContext) extends InjectedController {
   def frontend(path: String) = asset("nutria.html", "/public/html")
 
   def asset(file: String, folder: String) = assets.at(folder, file)
 
   def serviceWorker(file: String, folder: String) =
-    Action.async(
-      rq =>
-        assets
-          .at(folder, file)
-          .apply(rq)
-          .map(_.withHeaders("Service-Worker-Allowed" -> "/"))
-    )
+    env.mode match {
+      case Mode.Prod =>
+        Action.async(
+          rq =>
+            assets
+              .at(folder, file)
+              .apply(rq)
+              .map(_.withHeaders("Service-Worker-Allowed" -> "/"))
+        )
+      case _ => Action(NotFound)
+    }
 
 }
